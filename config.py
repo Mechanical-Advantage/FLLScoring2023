26b83a5311611b9a79f95406960de6a55ccdbca9
Keith White <az2k@verizon.net>
2023-11-16 20:24:21 -0500

    Update config and timer for correct date/time

--- config.py
+++ config.py
@@ -21,6 +21,6 @@ schedule_judging_outlength = 900  # secs, how long should the break between judg
 schedule_judging_teamgrace = 420  # secs, how long before or after judging should a team be excluded from matches?
 
 # General config for playoffs
-playoffs_starttime = 1700402400
+playoffs_starttime = 1700421300
 playoffs_cycletime = 600
 playoffs_matchnumberstart = 100
